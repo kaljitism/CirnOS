956392282880acf4bffca536b960696d0a462b2e
Aditya Dwivedi <dwikaljit@gmail.com>
2024-06-22 07:32:59 +0530

    Update default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -16,7 +16,7 @@ let
   impurity = inputs.impurity;
 in
 {
-  "CirnOS" = nixpkgs.lib.nixosSystem {
+  "nixos" = nixpkgs.lib.nixosSystem {
     specialArgs = { inherit inputs; };
     modules =
       [
@@ -27,16 +27,16 @@ in
           impurity.enable = true;
         }
 
-        ./CirnOS # this imports your entire host configuration in one swoop
+        ./nixos # this imports your entire host configuration in one swoop
 
         # this part is basically the same as putting configuration in your
         # configuration.nix, but is done on the topmost level for your convenience
         {
-          networking.hostName = "CirnOS";
-          _module.args = { username = "end"; };
+          networking.hostName = "nixos";
+          _module.args = { username = "aditya"; };
         }
       ]
       ++ homes; # imports the home-manager related configurations
   };
-  "CirnOS-impure" = self.nixosConfigurations."CirnOS".extendModules { modules = [{ impurity.enable = true; }]; };
+  "nixos-impure" = self.nixosConfigurations.nixos.extendModules { modules = [{ impurity.enable = true; }]; };
 }
